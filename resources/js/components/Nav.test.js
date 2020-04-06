1e7bee37407fb55161925a0f61cf907e62dbaae6
Wkan <wangkan1119@gmail.com>
2020-04-06 20:06:15 +0800

    fix test and refactor

--- resources/js/components/Nav.test.js
+++ resources/js/components/Nav.test.js
@@ -5,22 +5,29 @@ import { Router } from 'react-router-dom';
 import Nav from './Nav';
 
 describe('Nav', function () {
+    const renderNav = () => {
+        const history = createMemoryHistory({ initialEntries: ['/threads'] });
+        const utils = render(
+            <Router history={history}>
+                <Nav />
+            </Router>
+        );
+        return {
+            history,
+            ...utils,
+        };
+    };
+
     it('应该包括网站的名字', function () {
-        const { getByText } = render(<Nav />);
+        const { getByText, history } = renderNav();
 
         expect(getByText('Laravel Pro')).toBeVisible();
     });
 
     it('网站名字应该链接到首页', function () {
-        const history = createMemoryHistory({ initialEntries: ['/threads'] });
-        const { getByText } = render(
-            <Router history={history}>
-                <Nav />
-            </Router>
-        );
+        const { getByText, history } = renderNav();
 
         fireEvent.click(getByText('Laravel Pro'));
-
         expect(history.location.pathname).toBe('/');
     });
 });
