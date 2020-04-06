d409bcc59f3499fa744641fb4668eaa61b47b149
Wkan <wangkan1119@gmail.com>
2020-04-06 18:37:02 +0800

    add css transsform

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     moduleFileExtensions: ["js", "json", "jsx"],
     transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
     transform: {
-        "^.+\\.jsx?$": "<rootDir>/node_modules/babel-jest"
+        "^.+\\.jsx?$": "<rootDir>/node_modules/babel-jest",
+        "^.+\\.css$": "<rootDir>/resources/js/cssTransform.js",
     },
     setupFilesAfterEnv: ["<rootDir>/resources/js/testSetup.js"]
 };
