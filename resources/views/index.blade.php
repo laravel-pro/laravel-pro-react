cb7f2e93efef4a7610c3637e4a859bb4d16a912f
Wkan <wangkan1119@gmail.com>
2020-05-05 11:04:39 +0800

    add gtag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,15 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-80839283-3"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-80839283-3');
+        </script>
+
         <title>Laravel</title>
     </head>
     <body>
